a5fcf03baaeef152b17dd1661fba3819ef080b3b
DP-12 <admin@9476.cn>
2023-06-11 14:44:22 +0800

    modify sendNotify.js 2023-06-11 14:47:20

--- sendNotify.js
+++ sendNotify.js
@@ -278,7 +278,6 @@ if (process.env.SMTP_NAME) {
  * @param text 通知头
  * @param desp 通知体
  * @param params 某些推送通知方式点击弹窗可跳转, 例：{ url: 'https://abc.com' }
- * @param author 作者仓库等信息  例：`本通知 By：https://github.com/whyour/qinglong`
  * @returns {Promise<unknown>}
  */
 async function sendNotify(text, desp, params = {}) {
